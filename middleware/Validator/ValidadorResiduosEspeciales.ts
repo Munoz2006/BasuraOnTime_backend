93fd8181b5f579543915e670243d04cec8058a72
brayan aguirre <brayitaxX123@gmail.com>
2025-07-08 16:27:16 -0500

    feat: se organizo el error que generaba al registrar la solicitud

--- middleware/Validator/ValidadorResiduosEspeciales.ts
+++ middleware/Validator/ValidadorResiduosEspeciales.ts
@@ -3,9 +3,10 @@ import { Request, Response, NextFunction } from "express";
 
 export const validadorResiduosEspeciales: ValidationChain[] = [
     check('zona').isLength({ min: 5, max: 255 }),
+    check('fecha_solicitud').isDate(),
     check('cantidad').isNumeric(),
     check('tipo_residuo').isLength({ min: 3, max: 100 }),
-    check('tamano').isLength({ min: 3, max: 100 }),
+    check('tamano').isLength({ min: 3, max: 100 })
 ];
 
 export const validadorRE = (req: Request, res: Response, next: NextFunction) => {
